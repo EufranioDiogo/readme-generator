f3b2debcffa13e34e5ded774a09fa01648ec17f1
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-06-21 08:58:10 +0100

    feat: creating the templates at visual mode

--- src/main/java/com/readme/readmegenerator1/windows/selectTemplateWindow/SelectTemplateWindowApplication.java
+++ src/main/java/com/readme/readmegenerator1/windows/selectTemplateWindow/SelectTemplateWindowApplication.java
@@ -6,25 +6,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class SelectTemplateWindowApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader();
         Scene scene = new Scene(fxmlLoader.load(SelectTemplateWindowApplication.class.getResource("select-template-window.fxml")));
-
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         Path root = Paths.get("./resources");
 
         Files.walkFileTree(root, new ReadmeFileVisitor());
 
-        System.out.println(ReadmeFileVisitor.readmeSources);
-        System.out.println(ReadmeFileVisitor.readmeSources.size());
+
+
         stage.setScene(scene);
         stage.setTitle("Select the Template");
         stage.show();
+    }
+
+    private void generateTemplateViewElements() {
+
     }
 }
--- src/main/java/com/readme/readmegenerator1/windows/selectTemplateWindow/SelectTemplateWindowController.java
+++ src/main/java/com/readme/readmegenerator1/windows/selectTemplateWindow/SelectTemplateWindowController.java
@@ -1,19 +1,105 @@
 package com.readme.readmegenerator1.windows.selectTemplateWindow;
 
+import com.readme.logic.services.readme.ReadmeParam;
 import com.readme.logic.services.readme.ReadmeReader;
 import com.readme.logic.utils.files.ReadmeFileVisitor;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NavigableSet;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SelectTemplateWindowController {
     public VBox mainWindow;
+    public AnchorPane templatesContainer;
+    public static Path selectedReadme;
+    public static Path viewReadme;
+
+    private int templateLayoutX = 14;
+    private int templateLayoutY = 14;
+    private int numberOfTemplatesAdded = 0;
 
     public void readReadme(MouseEvent mouseEvent) {
-        System.out.println("Ola mundo");
+        NavigableSet<ReadmeParam> readmeParams = ReadmeReader.extractReadmeParam(ReadmeFileVisitor.readmeSources.get(0));
+    }
 
-        ReadmeReader.extractReadmeParam(ReadmeFileVisitor.readmeSources.get(0));
+    @FXML
+    protected void initialize() {
+        generateTemplateViewElements();
+    }
+
+    /*
+    * <AnchorPane layoutX="14.0" layoutY="14.0" prefHeight="287.0" prefWidth="345.0">
+           <children>
+              <Pane prefHeight="191.0" prefWidth="344.0" style="-fx-border-color: #222; -fx-border-radius: 10px;" />
+              <Label layoutX="1.0" layoutY="202.0" prefHeight="22.0" prefWidth="344.0" text="Readme File name">
+                 <font>
+                    <Font size="16.0" />
+                 </font>
+              </Label>
+              <Button layoutX="6.0" layoutY="246.0" mnemonicParsing="false" text="Selecionar" />
+              <Button layoutX="109.0" layoutY="246.0" mnemonicParsing="false" text="Ver" />
+           </children>
+        </AnchorPane>
+    * */
+    private void generateTemplateViewElements() {
+        Path root = Paths.get("./resources");
+        try {
+            Files.walkFileTree(root, new ReadmeFileVisitor());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ReadmeFileVisitor.readmeSources.forEach(path -> {
+            AnchorPane templateAnchorPane = new AnchorPane();
+            templateAnchorPane.getStyleClass().add("readme-template-container");
+            String background = ThreadLocalRandom.current().nextInt() + "";
+            templateAnchorPane.setStyle("-fx-background-color: #" + background.substring(0, 3) + ";");
+            Pane photoReadmeContainer = new Pane();
+
+            photoReadmeContainer.getStyleClass().add("readme-template-photo-container");
+
+            Label label = new Label();
+            label.getStyleClass().add("readme-template-name");
+            label.setText(path.getParent().toString());
+
+            Button selectButton = new Button();
+            selectButton.setText("Selecionar");
+            selectButton.setOnMouseClicked(event -> SelectTemplateWindowController.selectedReadme = path);
+
+            Button viewButton = new Button();
+
+            viewButton.setOnMouseClicked(event -> SelectTemplateWindowController.viewReadme = path);
+
+            selectButton.setText("Ver");
+
+            templateAnchorPane.getChildren().addAll(photoReadmeContainer, label, selectButton, viewButton);
+
+            templateAnchorPane.setLayoutX(templateLayoutX);
+            templateAnchorPane.setLayoutY(templateLayoutY);
+
+
+            numberOfTemplatesAdded++;
+
+            if (numberOfTemplatesAdded >= 2) {
+                // templateLayoutY = templateLayoutY + templateAnchorPaneHeight + space between
+                templateLayoutY = templateLayoutY + 287 + 20;
+                templateLayoutX = 14;
+            } else {
+                // templateLayoutX = templateLayoutX + templateAnchorPaneWith + space between
+                templateLayoutX = templateLayoutX + 345 + 20;
+            }
+            templatesContainer.getChildren().add(templateAnchorPane);
+        });
+
+        templatesContainer.setPrefHeight(templateLayoutY + 287 + 20);
     }
 }
